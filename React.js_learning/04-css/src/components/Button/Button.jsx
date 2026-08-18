abab8941ce7903266868069f3d3370d9a9e76f98
Shoeb Ansari <dragonwalker551@gmail.com>
2026-08-18 11:20:00 +0530

    how to use css module

---
+++ React.js_learning/04-css/src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import style from './button.module.css'
+const Button = () => {
+  return (
+    <div className={style.btn}>
+      Button
+    </div>
+  )
+}
+  export default Button
---
+++ React.js_learning/04-css/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import  style from './header.module.css'
+const Header = () => {
+  return (
+    <div className={style.box}>
+      <h1>Irfan Bhai</h1>
+      <button className={style.btn}>Button2</button>
+    </div>
+  )
+}
+
+export default Header
