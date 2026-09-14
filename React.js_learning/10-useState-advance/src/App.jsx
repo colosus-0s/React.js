9bd11fb4974d54890c95acbaccbe9e7d95da9aff
Shoeb Ansari <dragonwalker551@gmail.com>
2026-09-14 18:11:38 +0530

    useState batch update

--- React.js_learning/10-useState-advance/src/App.jsx
+++ React.js_learning/10-useState-advance/src/App.jsx
@@ -1,28 +1,28 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const App = () => {
+  const [num, setNum] = useState(10);
 
-  const [num, setNum] = useState([10,20,30,40])
+  let buttonClicked = () => {
+    
+    // setNum(num+1)
+    // setNum(num+1)
+    // setNum(num+1)
 
-  let buttonClicked = ()=>{
-    // const newNum = [...num]
-    // newNum.push(99)
-
-    // setNum(newNum)
-    // console.log(newNum)
-    setNum(newNum=>([...newNum,99]
-   
-    ))
-
-
-  }
+    setNum(prev=>(prev+1))
+    setNum(prev=>(prev+1))
+    setNum(prev=>(prev+1))
+  };
   return (
     <div>
-      <h1 className="m-10 text-6xl">{num.map((num)=>{
-        return num
-      })}</h1>
-      <button onClick={buttonClicked} className=" m-10 px-5 py-2 bg-amber-400 rounded-xl">click me</button>
+      <h1 className="m-10 text-6xl">{num}</h1>
+      <button
+        onClick={buttonClicked}
+        className=" m-10 px-5 py-2 bg-amber-400 rounded-xl"
+      >
+        click me
+      </button>
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
