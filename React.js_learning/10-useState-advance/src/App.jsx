05eefdebf760401d497d81a798161b866a440415
Shoeb Ansari <dragonwalker551@gmail.com>
2026-09-14 18:09:14 +0530

    useState with array

--- React.js_learning/10-useState-advance/src/App.jsx
+++ React.js_learning/10-useState-advance/src/App.jsx
@@ -2,26 +2,25 @@ import { useState } from "react"
 
 const App = () => {
 
-  const [num, setNum] = useState({user:"shoeb",age:20})
+  const [num, setNum] = useState([10,20,30,40])
 
   let buttonClicked = ()=>{
-    // const newNum = {...num}
-    // newNum.user = "Ansari"
-    // // console.log(newNum)
-    // setNum(newNum)
-    // // console.log(num)
-    // console.log(newNum)
+    // const newNum = [...num]
+    // newNum.push(99)
 
-    setNum(newNum=>({...newNum,
-        user:"Ansari",
-        age:21
-    }))
+    // setNum(newNum)
+    // console.log(newNum)
+    setNum(newNum=>([...newNum,99]
+   
+    ))
 
 
   }
   return (
     <div>
-      <h1 className="m-10 text-6xl">{num.user}, {num.age}</h1>
+      <h1 className="m-10 text-6xl">{num.map((num)=>{
+        return num
+      })}</h1>
       <button onClick={buttonClicked} className=" m-10 px-5 py-2 bg-amber-400 rounded-xl">click me</button>
     </div>
   )
